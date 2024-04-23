13ef989465498412394a76e242541c7d133ebfd4
RigobertoDavid <avilardavid@gmail.com>
2024-04-23 18:52:17 -0400

    whiles

---
+++ function.py
@@ -0,0 +1,46 @@
+
+def SumadeDos():
+    print("ingrese dos numeros")
+    num=int(input())
+    
+    num2=int(input())
+    print(num+num2)
+
+
+
+
+
+
+
+
+
+
+def Area():
+    print("ingrese ancho del rectangulo")
+    num=int(input())
+    print("ingrese largo del rectangulo")
+    num2=int(input())
+    resul=num*num2
+    print(num*num2)
+    return resul 
+
+
+
+
+def Perimetro():
+    print("ingrese ancho del rectangulo")
+    num=int(input())
+    print("ingrese largo del rectangulo")
+    num2=int(input())
+    resul=((num+num2)*2)
+    print("el perimetro es",resul)
+    return resul
+    
+
+
+Perimetro()
+
+
+
+
+
